4b797e661c5cb6973bb4eec754864a976b8fb5b4
unknown <xognsa110@gmail.com>
2020-06-01 15:25:34 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -3,11 +3,16 @@ package gittest;
 public class Hello {
 	public static void main(String[] args) {
 		hello();
-	
+		helloName("Kacao");
+		helloName("Elsa");
 	}
-	
 	public static void hello() {
 		System.out.println("Hello~");
 	}
+	public static void helloName(String name) {
+		System.out.println("Hello~ " + name);
+	}
+	
+
 
 }
